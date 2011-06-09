8630e691fa0ae43f575ef183b9b7c19c0dbb0ea0
NicolaCoppola <niko.cop@gmail.com>
2011-06-09 09:53:56 +0200

    Add translator supports : Portuguese thanks gugahoi

---
+++ language/Portuguese/Portuguese.php
@@ -0,0 +1,111 @@
+<?php
+// DATA section
+
+$giornosettimana = array (
+
+"Domingo",
+"Segunda",
+"Ter&ccedil;a", 
+"Quarta",
+"Quinta", 
+"Sexta", 
+"S&aacute;bado"
+
+);
+
+$nomemese = array (1 => 
+
+"Janeiro", 
+"Fevereiro", 
+"Mar&ccedil;o", 
+"Abril",
+"May", 
+"Junho", 
+"Julho", 
+"Agosto", 
+"Setembro", 
+"Outubro", 
+"Novembro", 
+"Dezembro"
+
+);
+
+// menu home page section 
+
+$t_meteo                ="CLIMA";
+$t_immagini 			="IMAGEM";
+$t_musica   			="M&Uacute;SICA";
+$t_video    			="V&Iacute;DEO";
+$t_remote   			="CONTROLE";
+$t_sistema  			="SISTEMA";
+
+// search and more 
+
+$t_back                 = "Voltar &agrave; pesquisa";
+$t_indietro             = "Voltar...";
+
+	// menu Music page section 
+	$t_music      			= "M&uacute;sica";
+	
+	$t_sfoglia_music        ="Navegar";
+	$t_cerca_music 			="Pesquisar";
+	$t_album_music   	    ="Album";
+	$t_artisti_music    	="Artistas";
+	$t_canzoni_music		="M&uacute;sicas";
+			
+		$t_cerca_musica     ="Pesquisar Tudo";
+		$t_cerca_album      ="Pesquisar Albuns";
+		$t_cerca_artista    ="Pesquisar Artistas";
+		$t_cerca_canzone    ="Pesquisar M&uacute;sicas";		
+	
+		//info Music
+		$t_artista_music	="Artistas";	
+	
+	// menu Video page section 
+	$t_videos      			="V&iacute;deos";
+	
+		$t_serietv     		="Shows de TV";
+		$t_stagionitv 		="Temporadas";
+		$t_episoditv 		="Epis&oacute;dios";
+		
+		$t_cerca_livetv     ="Pesquisar Canais";
+		$t_canali_livetv 	="Canais de TV";
+		$t_rec_livetv 		="Grava&ccedil;&otilde;es";
+		
+	$t_sfoglia_videos       ="Navegar";
+	$t_cerca_videos  		="Pesquisar";
+	$t_film_videos   	    ="Filmes";
+	$t_stagioni_videos     	="Seriados";
+	$t_musica_videos 		="V&iacute;deo Clipe";
+	$t_livetv_videos 		="TV ao Vivo";
+	$t_dvd_videos	 		="Tocar DVD ";
+	$t_blue_videos 			="Tocar Blu-Ray";
+	
+		$t_cerca_video      ="Pesquisar Tudo";
+		$t_cerca_film       ="Pesquisar Filmes";
+		$t_cerca_serietv    ="Pesquisar Seriados";
+		$t_cerca_musicvideo ="Pesquisar Video Clipes";		
+	
+		//info Video 
+		$t_stagione_videos   ="Temporada";
+		$t_regista_videos  	 ="Diretor";
+		$t_genere_videos   	 ="G&ecirc;nero";
+		
+		
+		
+	
+	// menu System page section 
+	$t_sistem      			="Configura&ccedil;&otilde;es";
+	$t_lingua_sistem        ="Idioma";
+	$t_host_sistem 		    ="Conex&atilde;o";
+	$t_meteo_sistem   	    ="Clima";
+	$t_image_sistem    	    ="Imagem";
+	$t_audio_sistem		    ="Audio";
+	$t_video_sistem   	    ="Video";
+	$t_skin_sistem    	    ="Skin";
+	$t_about_sistem		    ="Sobre o Autor";
+
+
+
+
+?> 
