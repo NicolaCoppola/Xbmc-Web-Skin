85b8ed404d2a6bda3d7867f34f35e5b06b42a23a
gugahoi <guga@maurorisch.com.br>
2011-06-10 16:40:17 +1000

    Added cURL check to index.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php if(!extension_loaded('curl')){echo "<script>alert('cURL not found. Please make sure your web server is compatible with cURL and it is enabled.')</script>";} ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
